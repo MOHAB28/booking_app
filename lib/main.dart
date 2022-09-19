abdf240d37bfd57a962af6d5d19be91eaa2f356f
MOHAB28 <mohabmahmo@gmail.com>
2022-09-19 23:10:12 +0200

    test with aly

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                   );
                 },
-                child: const Text('Login Button'),
+                child: const Text('Login Button with aly'),
               ),
             ],
           );
